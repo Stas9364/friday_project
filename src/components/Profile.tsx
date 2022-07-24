d3c280abf9d74157f266d5b5635d1d472f8dab83
Denisssska <yarmoshkoden18m@gmail.com>
2022-07-24 07:08:08 +0300

    new branch profile

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
-
+import style from './profile.module.css'
 const Profile = () => {
     return (
-        <div>
-            PROFILE
+        <div className={style.profile}>
+            <div className={style.border}>
+                <h1>Personal information</h1>
+                <img src="" alt="'some img"/>
+                <h2>name</h2>
+                <h2>email</h2>
+                <button>logOut</button>
+            </div>
+
         </div>
     );
 };
